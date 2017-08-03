69ee5141f461c99d74893fd086283c00cde1346b
JaimeVelasco <contact@jaimevelas.co>
2017-08-02 21:57:41 -0500

    Email collector working

---
+++ main.js
@@ -0,0 +1,97 @@
+$(".burger").click(function() {
+		$(".burger").toggleClass("is-active");
+		$(".navbar-menu").toggleClass("is-active");
+});
+
+$("#products").click(function() {
+		$('html, body').animate({
+				scrollTop: $(".bv_pens").offset().top
+		}, 2000);
+		$(".burger").removeClass("is-active");
+		$(".navbar-menu").removeClass("is-active");
+});
+
+$("#shop").click(function() {
+		$('html, body').animate({
+				scrollTop: $(".bv_shop").offset().top
+		}, 2000);
+		$(".burger").removeClass("is-active");
+		$(".navbar-menu").removeClass("is-active");
+});
+
+
+$("#events").click(function() {
+		$('html, body').animate({
+				scrollTop: $(".bv_events").offset().top
+		}, 2000);
+		$(".burger").removeClass("is-active");
+		$(".navbar-menu").removeClass("is-active");
+});
+
+
+$("#locator").click(function() {
+		$('html, body').animate({
+				scrollTop: $(".bv_locate").offset().top
+		}, 2000);
+		$(".burger").removeClass("is-active");
+		$(".navbar-menu").removeClass("is-active");
+});
+
+
+$("#faq").click(function() {
+		$('html, body').animate({
+				scrollTop: $(".bv_faq").offset().top
+		}, 2000);
+		$(".burger").removeClass("is-active");
+		$(".navbar-menu").removeClass("is-active");
+});
+
+setTimeout(function () {
+	$(".modal").addClass("is-active");
+}, 1000);
+
+
+$('.modal-close, .noThanks, #formSubmit').click(function() {
+		$(".modal").removeClass("is-active");
+});
+
+
+$("#formSubmit").click(function() {
+	// get email, zip and name to be posted
+	var email = document.getElementById('email').value
+	var zipCode = document.getElementById('zipCode').value
+	var name = document.getElementById('name').value
+	// format payload for zapier
+	var zdata = formatForZapier(email, zipCode, name)
+	// log in console
+	console.log(zdata)
+	// post
+	 $.ajax({
+		 // url is what you get from activating the "Incoming WebHooks" slack integration
+		 // if you leave, you should see an error message "No Team", status 404
+		 url: 'https://hooks.zapier.com/hooks/catch/2256084/5e9szr/',
+		 type: 'POST',
+		 processData: true,
+		 data : zdata ,
+		 success : function(data) {
+			 // success will show on page
+			 console.log(data)
+		 },
+		 error: function(data){
+			 // error will show error object
+			 console.log(data)
+		}
+	 });
+
+  function formatForZapier(email, name, zipCode){
+		var date = new Date()
+    var payload ={
+      "email": email,
+			"name": name,
+			"zipCode": zipCode,
+			"date": date.toLocaleString('en-US')
+    };
+    // return json string of payload
+    return JSON.stringify(payload)
+  }
+});

60d9353ca3d38df61bba9dd4c03e3ad19799c4d2
JaimeVelasco <contact@jaimevelas.co>
2017-08-03 18:43:01 -0500

    mobile metadata

--- main.js
+++ main.js
@@ -12,9 +12,11 @@ $(".burger").click(function() {
 });
 
 
-// Main Navigation scroll scripts
+// *** Main Navigation scroll scripts
 // Each triggered by a nav button
 // TODO: Consolidate in a more efficient way. Maybe switch/case?
+// ***
+
 $("#products").click(function() {
 		$('html, body').animate({
 				scrollTop: $(".bv_pens").offset().top

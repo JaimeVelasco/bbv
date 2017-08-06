84f95537d8964ef562bb84eb95434114ec8f81e7
JaimeVelasco <contact@jaimevelas.co>
2017-08-05 20:53:57 -0500

    Added signup for logic, hide when logged-in

--- main.js
+++ main.js
@@ -23,10 +23,47 @@ var uiConfig = {
 	tosUrl: '/tos'
 };
 
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
+
+
+initApp = function() {
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			// User is signed in.
+			var displayName = user.displayName;
+			var email = user.email;
+			var emailVerified = user.emailVerified;
+			var photoURL = user.photoURL;
+			var uid = user.uid;
+			var phoneNumber = user.phoneNumber;
+			var providerData = user.providerData;
+			user.getIdToken().then(function(accessToken) {
+				document.getElementById('account-details').textContent = "Welcome " + displayName
+			});
+		} else {
+			// User is signed out.
+			document.getElementById('account-details').textContent = 'Please Log in';
+		}
+	}, function(error) {
+		console.log(error);
+	});
+};
+
+window.addEventListener('load', function() {
+	initApp()
+});
+
+
+
+
+var user = firebase.auth().currentUser;
+if (user) {
+  // User is signed in.
+} else {
+	// Initialize the FirebaseUI Widget using Firebase.
+	var ui = new firebaseui.auth.AuthUI(firebase.auth());
+	// The start method will wait until the DOM is loaded.
+	ui.start('#firebaseui-auth-container', uiConfig);
+}
 
 
 

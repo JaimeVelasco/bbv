25197eddcdcc6f6371f4ed60b36925305ed496ed
JaimeVelasco <contact@jaimevelas.co>
2017-09-18 13:53:44 -0500

    User gets feedback when submitting email

--- main.js
+++ main.js
@@ -1,84 +1,14 @@
- // // Initialize Firebase
-// var config = {
-// 	apiKey: "AIzaSyDTchze2Ms9gTlcaJW8J4wrP5BgyrQ_wlw",
-// 	authDomain: "bumblebee-2053b.firebaseapp.com",
-// 	databaseURL: "https://bumblebee-2053b.firebaseio.com",
-// 	projectId: "bumblebee-2053b",
-// 	storageBucket: "bumblebee-2053b.appspot.com",
-// 	messagingSenderId: "847168397202"
-// };
-//
-// firebase.initializeApp(config);
-//
-// // Get a reference to the database service
-// var database = firebase.database();
-//
-// return firebase.database().ref('/content/').once('value').then(function(snapshot) {
-//   var username = snapshot.val().username;
-// 	console.log("username", username);
-//   // ...
-// });
-//
-//
-// // FirebaseUI config.
-// var uiConfig = {
-// 	signInSuccessUrl: '/bbv/admin.html',
-// 	signInOptions: [
-// 		// Leave the lines as is for the providers you want to offer your users.
-// 		firebase.auth.GoogleAuthProvider.PROVIDER_ID
-// 	],
-// 	// Terms of service url.
-// 	tosUrl: '/tos'
-// }
-//
-// initApp = function() {
-// 	firebase.auth().onAuthStateChanged(function(user) {
-// 		var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// 		if (user) {
-// 			// User is signed in.
-// 			var displayName = user.displayName;
-// 			var email = user.email;
-// 			var emailVerified = user.emailVerified;
-// 			var photoURL = user.photoURL;
-// 			var uid = user.uid;
-// 			var phoneNumber = user.phoneNumber;
-// 			var providerData = user.providerData;
-// 			user.getIdToken().then(function(accessToken) {
-// 				document.getElementById('account-details').textContent = "Welcome " + displayName
-// 			});
-// 			// Show login form only if user is not logged in
-// 			// Remove loginUI, wait for login to finish
-// 			ui.reset('#firebaseui-auth-container');
-// 		} else {
-// 			// User is signed out.
-// 			document.getElementById('account-details').textContent = 'Please Log in';
-// 			// Initialize the FirebaseUI Widget using Firebase.
-// 			// The start method will wait until the DOM is loaded.
-// 			ui.start('#firebaseui-auth-container', uiConfig);
-// 		}
-// 	}, function(error) {
-// 		console.log(error);
-// 	});
-// };
-//
-// window.addEventListener('load', function() {
-// 	initApp()
-// });
-//
-
-
-
 // Get video aligned to top and preserve aspect-ratio on all device sizes
 var distance = $('.bv_mission').offset().top,
 $window = $(window);
 $window.scroll(function() {
 $window.scrollTop()
-  if ( $window.scrollTop() >= distance ) {
-      $('.fullscreen-bg').css('display', 'none');
-  }
-  else  {
-      $('.fullscreen-bg').css('display', 'block');
-  }
+ if ( $window.scrollTop() >= distance ) {
+		 $('.fullscreen-bg').css('display', 'none');
+ }
+ else  {
+		 $('.fullscreen-bg').css('display', 'block');
+ }
 });
 
 
@@ -87,190 +17,195 @@ $window.scrollTop()
 // Age verification first, then email collector.
 // Trigger Email modal if browser does not have cookie
 if (localStorage.getItem("bumbleBee") !== "true" && $("#minAgeCheck").hasClass("is-active") === false ) {
-	setTimeout(function () {
-		$("#collector").addClass("is-active");
-	}, 2000);
+ setTimeout(function () {
+	 $("#collector").addClass("is-active");
+ }, 2000);
 };
 
 if (localStorage.getItem("overage") === "true") {
-	$("#minAgeCheck").removeClass("is-active");
-	if (localStorage.getItem("bumbleBee") !== "true") {
-		setTimeout(function () {
-			$("#collector").addClass("is-active");
-		}, 2000);
-	};
+ $("#minAgeCheck").removeClass("is-active");
+ if (localStorage.getItem("bumbleBee") !== "true") {
+	 setTimeout(function () {
+		 $("#collector").addClass("is-active");
+	 }, 2000);
+ };
 } else {
-	$("#minAgeCheck").addClass("is-active");
+ $("#minAgeCheck").addClass("is-active");
 }
 
 // Close minAgeCheck
 $('#over').click(function() {
-		$(".modal").removeClass("is-active");
-		localStorage.setItem("overage", true);
-		if (localStorage.getItem("bumbleBee") !== "true") {
-			setTimeout(function () {
-				$("#collector").addClass("is-active");
-			}, 2000);
-		};
+	 $(".modal").removeClass("is-active");
+	 localStorage.setItem("overage", true);
+	 if (localStorage.getItem("bumbleBee") !== "true") {
+		 setTimeout(function () {
+			 $("#collector").addClass("is-active");
+		 }, 2000);
+	 };
 });
 
 $('#under').click(function() {
-		$(".minAgeCheck").addClass("hidden");
-		$(".underAge").removeClass("hidden");
-		setTimeout(function () {
-			window.location.href = "http://www.apa.org/monitor/2015/11/marijuana-brain.aspx";
-		}, 3000);
+	 $(".minAgeCheck").addClass("hidden");
+	 $(".underAge").removeClass("hidden");
+	 setTimeout(function () {
+		 window.location.href = "http://www.apa.org/monitor/2015/11/marijuana-brain.aspx";
+	 }, 3000);
 });
 
 
 // Toggle mobile menu
 $(".burger").click(function() {
-		$(".burger").toggleClass("is-active");
-		$(".navbar-menu").toggleClass("is-active");
+	 $(".burger").toggleClass("is-active");
+	 $(".navbar-menu").toggleClass("is-active");
 });
 
 
 
 
 $("#products").click(function() {
-		$('html, body').animate({
-				scrollTop: $(".bv_pens").offset().top
-		}, 2000);
-		$(".burger").removeClass("is-active");
-		$(".navbar-menu").removeClass("is-active");
+	 $('html, body').animate({
+			 scrollTop: $(".bv_pens").offset().top
+	 }, 2000);
+	 $(".burger").removeClass("is-active");
+	 $(".navbar-menu").removeClass("is-active");
 });
 
 $("#shop").click(function() {
-		$('html, body').animate({
-				scrollTop: $(".bv_shop").offset().top
-		}, 2000);
-		$(".burger").removeClass("is-active");
-		$(".navbar-menu").removeClass("is-active");
+	 $('html, body').animate({
+			 scrollTop: $(".bv_shop").offset().top
+	 }, 2000);
+	 $(".burger").removeClass("is-active");
+	 $(".navbar-menu").removeClass("is-active");
 });
 
 $("#events").click(function() {
-		$('html, body').animate({
-				scrollTop: $(".bv_events").offset().top
-		}, 2000);
-		$(".burger").removeClass("is-active");
-		$(".navbar-menu").removeClass("is-active");
+	 $('html, body').animate({
+			 scrollTop: $(".bv_events").offset().top
+	 }, 2000);
+	 $(".burger").removeClass("is-active");
+	 $(".navbar-menu").removeClass("is-active");
 });
 
 $("#locator").click(function() {
-		$('html, body').animate({
-				scrollTop: $(".bv_locate").offset().top
-		}, 2000);
-		$(".burger").removeClass("is-active");
-		$(".navbar-menu").removeClass("is-active");
+	 $('html, body').animate({
+			 scrollTop: $(".bv_locate").offset().top
+	 }, 2000);
+	 $(".burger").removeClass("is-active");
+	 $(".navbar-menu").removeClass("is-active");
 });
 
 $("#faq").click(function() {
-		$('html, body').animate({
-				scrollTop: $(".bv_faq").offset().top
-		}, 2000);
-		$(".burger").removeClass("is-active");
-		$(".navbar-menu").removeClass("is-active");
+	 $('html, body').animate({
+			 scrollTop: $(".bv_faq").offset().top
+	 }, 2000);
+	 $(".burger").removeClass("is-active");
+	 $(".navbar-menu").removeClass("is-active");
 });
 
 
 // Close emailCollector
 $('.modal-close, .noThanks').click(function() {
-		$(".modal").removeClass("is-active");
+	 $(".modal").removeClass("is-active");
 });
 
 // Remove cookie if user declines email form
 $('.noThanks').click(function() {
-		localStorage.setItem("bumbleBee", true);
+	 localStorage.setItem("bumbleBee", true);
 });
 
 
 // Pens info
 $('.pen1').click(function() {
-		$("#pen1").addClass("is-active");
+	 $("#pen1").addClass("is-active");
 });
 
 $('.pen2').click(function() {
-		$("#pen2").addClass("is-active");
+	 $("#pen2").addClass("is-active");
 });
 
 $('.pen3').click(function() {
-		$("#pen3").addClass("is-active");
+	 $("#pen3").addClass("is-active");
 });
+
+
+
+// FAQ tabs
+$('ul.tabsjs li').click(function(){
+	var tab_id = $(this).attr('data-tab');
+	$('ul.tabsjs li').removeClass('is-active');
+	$('.tab-content').removeClass('is-active');
+	$(this).addClass('is-active');
+	$("#"+tab_id).addClass('is-active');
+});
+
 
 
 // Submit email form
 $("#formSubmit").click(function(event) {
-	// get email, zip and name to be posted
-	var email = document.getElementById('email').value
-	var zipCode = document.getElementById('zipCode').value
-	var name = document.getElementById('name').value
-	// format payload for zapier
-	var zdata = formatForZapier(email, zipCode, name)
-	// If no empty fields
-	if (email !== "" && zipCode !== "" && name !== "") {
-		if (validateEmail(email)) {
-			 event.preventDefault()
-			// post to Zapier webhook
-			$.ajax({
-				url: 'https://hooks.zapier.com/hooks/catch/2256084/5e9szr/',
-				type: 'POST',
-				processData: true,
-				data : zdata ,
-				success : function(data) {
-					console.log("success", data)
-					// reload/submit
-					$("#pincheForm").submit()
-					// create cookie
-					localStorage.setItem("bumbleBee", true);
-				},
-				error: function(data){
-					console.log("error", data)
-				}
-			});
-		} else if (!validateEmail(email)){
-			// Validate Email form (on top of HTML5's built in validation)
-			var element = document.getElementById("email");
-				element.oninvalid = function(e) {
-						e.target.setCustomValidity("");
-						if (!e.target.validity.valid) {
-								e.target.setCustomValidity("Something is wrong with your email");
-						}
-				};
-				element.oninput = function(e) {
-						e.target.setCustomValidity("");
-				};
-		}
-	}
+ // get email, zip and name to be posted
+ var email = document.getElementById('email').value
+ var zipCode = document.getElementById('zipCode').value
+ var name = document.getElementById('name').value
+ // format payload for zapier
+ var zdata = formatForZapier(email, zipCode, name)
+ // If no empty fields
+ if (email !== "" && zipCode !== "" && name !== "") {
+	 if (validateEmail(email)) {
+			event.preventDefault()
+		 // post to Zapier webhook
+		 $.ajax({
+			 url: 'https://hooks.zapier.com/hooks/catch/2256084/5e9szr/',
+			 type: 'POST',
+			 processData: true,
+			 data : zdata ,
+			 success : function(data) {
+				//  Provide feedback to user
+				 $(".collectContainer").addClass("hidden")
+				 $(".sentMessage").removeClass("hidden")
+
+				//  Set localStorage item on browser
+				 localStorage.setItem("bumbleBee", true);
+
+				 // reload/submit after 3 seconds
+				 setTimeout(function () {
+					 $("#pincheForm").submit()
+				 }, 3000);
+			 },
+			 error: function(data){
+				 console.log("error", data)
+			 }
+		 });
+	 } else if (!validateEmail(email)){
+		 // Validate Email form (on top of HTML5's built in validation)
+		 var element = document.getElementById("email");
+			 element.oninvalid = function(e) {
+					 e.target.setCustomValidity("");
+					 if (!e.target.validity.valid) {
+							 e.target.setCustomValidity("Something is wrong with your email");
+					 }
+			 };
+			 element.oninput = function(e) {
+					 e.target.setCustomValidity("");
+			 };
+	 }
+ }
 
 
-	function validateEmail(email) {
-			var regex =  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-	    return regex.test(email);
-	}
+ function formatForZapier(email, zipCode, name){
+	 var date = new Date()
+	 var payload ={
+		 "email": email,
+		 "name": name,
+		 "zipCode": zipCode,
+		 "date": date.toLocaleString('en-US')
+	 };
+	 // return json string of payload
+	 return JSON.stringify(payload)
+ }
 
-
-  function formatForZapier(email, zipCode, name){
-		var date = new Date()
-    var payload ={
-      "email": email,
-			"name": name,
-			"zipCode": zipCode,
-			"date": date.toLocaleString('en-US')
-    };
-    // return json string of payload
-    return JSON.stringify(payload)
-  }
+ function validateEmail(email) {
+		 var regex =  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+		 return regex.test(email);
+ }
 });
 // END OF SUBMIT FORM event
-
-// FAQ tabs
-$(document).ready(function(){
-	$('ul.tabsjs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
-		console.log("tab_id");
-		$('ul.tabsjs li').removeClass('is-active');
-		$('.tab-content').removeClass('is-active');
-		$(this).addClass('is-active');
-		$("#"+tab_id).addClass('is-active');
-	});
-});
